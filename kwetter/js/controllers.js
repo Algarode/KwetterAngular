5be8e0179d9c89b94f65e6ce623a9fed9182b40e
Edwin Lambregts <e.lambregts@student.fontys.nl>
2016-05-18 08:54:56 +0200

    Initial commit

---
+++ kwetter/js/controllers.js
@@ -0,0 +1,22 @@
+angular.module('app.controllers', [])
+  
+.controller('homeCtrl', function($scope) {
+
+})
+   
+.controller('profilesCtrl', function($scope) {
+
+})
+   
+.controller('timelineCtrl', function($scope) {
+
+})
+      
+.controller('profilePageCtrl', function($scope) {
+
+})
+   
+.controller('profileEditorCtrl', function($scope) {
+
+})
+ 
